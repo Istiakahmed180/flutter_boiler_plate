0e6d03fefe1372cbaa84469bc60bc76e4381b64e
Flutter Assignment 2 <Istiakahmed180@gmail.com>
2024-08-08 00:23:31 +0600

    Added toast helper class

---
+++ lib/constants/api_path.dart
@@ -0,0 +1,5 @@
+class ApiPath {
+  static const String baseUrl = 'https://api.example.com';
+  static const String login = '$baseUrl/login';
+  static const String register = '$baseUrl/register';
+}
